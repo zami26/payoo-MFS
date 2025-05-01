bbb10eb0398253861b9dd61a3c6a24ae8cd65466
zami26 <arafatulislamzami@gmail.com>
2025-05-01 22:46:35 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("login").addEventListener("click",function(event){
+    event.preventDefault();
+    number=document.getElementById("phone-number").value;
+    console.log(number);
+});
